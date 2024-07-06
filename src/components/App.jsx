c0de7721045a0051e1bcbec1d3aeb059ab344566
Neoriya <alena.neori@gmail.com>
2024-07-07 02:11:11 +0300

    basic form for task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,66 @@
-import Header from "./Header/Header";
-import Body from "./Body/Body";
-import Message from "./Message/Message";
-import Footer from "./Footer/Footer";
+// import Header from "./Header/Header";
+// import Body from "./Body/Body";
+// import Message from "./Message/Message";
+// import Footer from "./Footer/Footer";
+
+import Profile from "./Profile/Profile";
+// import FriendList from "./FriendList/FriendList";
+// import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import userData from "../userData";
+
+// const userData = {
+//   username: "Jacques Gluke",
+//   tag: "jgluke",
+//   location: "Ocho Rios, Jamaica",
+//   avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
 
 const App = () => {
-  const message = "Hello!";
-  const isOnline = true;
-  const age = 21;
-  const fruits = ["banana", "aple", "lemon"];
+  // const message = "Hello!";
+  // const isOnline = true;
+  // const age = 21;
+  // const fruits = ["banana", "aple", "lemon"];
 
   return (
-    <main>
-      <Header />
-      <Body />
-      <Message autor="Htnhj" message={message} value={[1, 2, 3]} />
-      <Message
-        autor="Ghjkfgyh"
-        message="fghbnhgf yhjhghjhghj fgbhnbfgbhgfdf lkjhbnjk lkjhk pokjh"
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
       />
-      <Message message="fghjkl;lkmjhgfgyhujkl;';lkjhgfghujikol;'" />
+      {/* <FriendList />
+      <TransactionHistory /> */}
+    </>
 
-      {isOnline && <h1>Welcome</h1>}
-      {!isOnline && <h1>Login</h1>}
-      {age >= 18 ? <h1>Welcome</h1> : <h1>Login</h1>}
+    // <main>
+    //   <Header />
+    //   <Body />
+    //   <Message autor="Htnhj" message={message} value={[1, 2, 3]} />
+    //   <Message
+    //     autor="Ghjkfgyh"
+    //     message="fghbnhgf yhjhghjhghj fgbhnbfgbhgfdf lkjhbnjk lkjhk pokjh"
+    //   />
+    //   <Message message="fghjkl;lkmjhgfgyhujkl;';lkjhgfghujikol;'" />
 
-      <ul>
-        {fruits.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+    //   {isOnline && <h1>Welcome</h1>}
+    //   {!isOnline && <h1>Login</h1>}
+    //   {age >= 18 ? <h1>Welcome</h1> : <h1>Login</h1>}
 
-      <Footer />
-    </main>
+    //   <ul>
+    //     {fruits.map((item) => (
+    //       <li key={item}>{item}</li>
+    //     ))}
+    //   </ul>
+
+    //   <Footer />
+    // </main>
   );
 };
 
